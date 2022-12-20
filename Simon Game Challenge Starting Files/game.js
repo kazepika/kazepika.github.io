321f41e23b8944dc8d801febdb896fd6be224fb9
kazepika <kazapika@gmail.com>
2022-12-20 14:40:52 +0800

    fix bug
    moving the reset user clicked list from checking function to start sequence function to prevent the problem of cheating by overclicking the same button

--- Simon Game Challenge Starting Files/game.js
+++ Simon Game Challenge Starting Files/game.js
@@ -13,39 +13,42 @@ function playSound(name) {
 
 function animatePress(currentColour) {
     $(`#${currentColour}`).addClass("pressed");
-    setTimeout(function(){
+    setTimeout(function () {
         $(`#${currentColour}`).removeClass("pressed");
-    }, 100)
+    }, 100);
 }
 
 function nextSequence() {
-    let randomNumber = Math.round(Math.random() * 3) // 0 - 3 integers
-    let randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour)
-    
-    
-    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100); 
-
-    playSound(randomChosenColour)
-    level += 1;
+    userClickedPattern = [];
+    level++;
     $("#level-title").text(`Level ${level}`);
+    let randomNumber = Math.round(Math.random() * 3); // 0 - 3 integers
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
 }
 
-$('.btn').click(function(id) {
+$('.btn').click(function (id) {
     let userChosenColour = $(this).attr("id");
     // let userChosenColour = id.target.id;
 
-    animatePress(userChosenColour)
-    // $(`#${userChosenColour}`).fadeOut(100).fadeIn(100);
     userClickedPattern.push(userChosenColour);
+
+    animatePress(userChosenColour);
+    // $(`#${userChosenColour}`).fadeOut(100).fadeIn(100);
+
     playSound(userChosenColour);
-    checkAnswer(level);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
 
 // detect keyboard press for the first time, can be considered as the most important part of the codes
-    $(document).keypress(function() {
-        if (!started) {
+$(document).keypress(function () {
+    if (!started) {
         $("#level-title").text(`Level ${level}`);
         nextSequence();
         started = true;
@@ -53,34 +56,34 @@ $('.btn').click(function(id) {
 });
 
 // check if the answer match
-function checkAnswer(currentLevel){
-    if (gamePattern[userClickedPattern.length - 1] === userClickedPattern[userClickedPattern.length - 1]){
+function checkAnswer(currentLevel) {
+
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
         console.log("success");
+        // check if cleared, if yes, go to next level
+        if (userClickedPattern.length === gamePattern.length) {
+            console.log("finished");
+            setTimeout(function () {
+                nextSequence();
+            }, 1000)
+        }
     }
     else { // if wrong
         console.log("wrong");
         playSound("wrong");
         $("body").addClass("game-over");
-        setTimeout(function(){
+        setTimeout(function () {
             $("body").removeClass("game-over");
         }, 200)
         $("h1").text("Game Over, Press Any Key to Restart")
+
         startOver();
-    }
-    // check if cleared, if yes, go to next level
-    if (userClickedPattern.length == currentLevel) {
-        console.log("finished");
-        setTimeout(function(){
-            nextSequence();
-        }, 1000)
-        userClickedPattern = [];
     }
 }
 
 // reset the value to just started
-function startOver(){
+function startOver() {
     level = 0;
     gamePattern = [];
-    userClickedPattern = [];
     started = false;
 }
